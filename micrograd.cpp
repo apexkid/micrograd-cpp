65373f24ba445552244edde35ef91132452d2660
apexkid <apexkid369@gmail.com>
2025-01-02 18:23:43 -0800

    Renamed Value to GradNode

--- micrograd.cpp
+++ micrograd.cpp
@@ -6,52 +6,55 @@
 #include <string>
 #include <vector>
 
-class Value {
+class GradNode {
 public:
-  Value(double data, std::string label,
-        std::vector<std::shared_ptr<Value>> children,
-        std::function<void(std::vector<std::shared_ptr<Value>>)> backward_fn) {
+  GradNode(
+      double data, std::string label,
+      std::vector<std::shared_ptr<GradNode>> children,
+      std::function<void(std::vector<std::shared_ptr<GradNode>>)> backward_fn) {
     this->data = data;
     this->label = label;
     this->children = children;
     this->backward_fn = backward_fn;
   }
 
-  Value(double data, std::string label) {
+  GradNode(double data, std::string label) {
     this->data = data;
     this->label = label;
   }
 
   void make_scalar() { is_scalar = true; }
 
-  static std::shared_ptr<Value> create_value(double data, std::string label) {
-    return std::make_shared<Value>(data, label);
+  static std::shared_ptr<GradNode> create_grad_node(double data,
+                                                    std::string label) {
+    return std::make_shared<GradNode>(data, label);
   }
 
-  static std::shared_ptr<Value> create_value(
+  static std::shared_ptr<GradNode> create_grad_node(
       double data, std::string label,
-      std::vector<std::shared_ptr<Value>> children,
-      std::function<void(std::vector<std::shared_ptr<Value>>)> backward_fn) {
-    return std::make_shared<Value>(data, label, children, backward_fn);
+      std::vector<std::shared_ptr<GradNode>> children,
+      std::function<void(std::vector<std::shared_ptr<GradNode>>)> backward_fn) {
+    return std::make_shared<GradNode>(data, label, children, backward_fn);
   }
 
-  friend std::shared_ptr<Value> operator+(double a,
-                                          const std::shared_ptr<Value> &b) {
-    auto value = create_value(a, std::to_string(a));
-    value->make_scalar();
-    return value + b;
+  friend std::shared_ptr<GradNode>
+  operator+(double a, const std::shared_ptr<GradNode> &b) {
+    auto gradnode = create_grad_node(a, std::to_string(a));
+    gradnode->make_scalar();
+    return gradnode + b;
   }
 
-  friend std::shared_ptr<Value> operator+(const std::shared_ptr<Value> &a,
-                                          double b) {
-    auto value = create_value(b, std::to_string(b));
-    value->make_scalar();
-    return a + value;
+  friend std::shared_ptr<GradNode> operator+(const std::shared_ptr<GradNode> &a,
+                                             double b) {
+    auto gradnode = create_grad_node(b, std::to_string(b));
+    gradnode->make_scalar();
+    return a + gradnode;
   }
 
-  friend std::shared_ptr<Value> operator+(const std::shared_ptr<Value> &a,
-                                          const std::shared_ptr<Value> &b) {
-    auto output_backward = [](std::vector<std::shared_ptr<Value>> children) {
+  friend std::shared_ptr<GradNode>
+  operator+(const std::shared_ptr<GradNode> &a,
+            const std::shared_ptr<GradNode> &b) {
+    auto output_backward = [](std::vector<std::shared_ptr<GradNode>> children) {
       for (auto &child : children) {
         if (!child->is_scalar) {
           child->grad += 1.0;
@@ -61,9 +64,9 @@ public:
 
     auto output_data = a->data + b->data;
     auto output_label = a->label + "+" + b->label;
-    auto output_children = std::vector<std::shared_ptr<Value>>{a, b};
-    return create_value(output_data, output_label, output_children,
-                        output_backward);
+    auto output_children = std::vector<std::shared_ptr<GradNode>>{a, b};
+    return create_grad_node(output_data, output_label, output_children,
+                            output_backward);
   }
 
   void backward() {
@@ -91,16 +94,17 @@ public:
   }
 
 private:
-  std::stack<const Value *> TopologicalSort() {
-    std::stack<const Value *> stack;
-    std::set<const Value *> visited;
+  std::stack<const GradNode *> TopologicalSort() {
+    std::stack<const GradNode *> stack;
+    std::set<const GradNode *> visited;
 
     TopologicalSortUtil(*this, stack, visited);
     return stack;
   }
 
-  void TopologicalSortUtil(const Value &node, std::stack<const Value *> &stack,
-                           std::set<const Value *> &visited) {
+  void TopologicalSortUtil(const GradNode &node,
+                           std::stack<const GradNode *> &stack,
+                           std::set<const GradNode *> &visited) {
     if (visited.find(&node) != visited.end()) {
       return;
     }
@@ -113,22 +117,22 @@ private:
     visited.emplace(&node);
   }
 
-  std::function<void(std::vector<std::shared_ptr<Value>>)> backward_fn;
+  std::function<void(std::vector<std::shared_ptr<GradNode>>)> backward_fn;
 
   double data;
   double grad = 0.0;
-  std::vector<std::shared_ptr<Value>> children;
+  std::vector<std::shared_ptr<GradNode>> children;
   std::string label;
   bool is_scalar = false;
 };
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
-  auto a = Value::create_value(1.0, "a");
-  auto b = Value::create_value(2.0, "b");
-  auto c = Value::create_value(3.0, "c");
-  std::shared_ptr<Value> d = a + b;
-  std::shared_ptr<Value> e = 1 + a + d + c;
+  auto a = GradNode::create_grad_node(1.0, "a");
+  auto b = GradNode::create_grad_node(2.0, "b");
+  auto c = GradNode::create_grad_node(3.0, "c");
+  std::shared_ptr<GradNode> d = a + b;
+  std::shared_ptr<GradNode> e = 1 + a + d + c;
   e->backward();
   e->print_network();
   return 0;
