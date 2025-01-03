9802c003b223dd511e90aa91ad9b608192f5b1b9
apexkid <apexkid369@gmail.com>
2025-01-02 18:20:47 -0800

    Working code with proper grads for addition

--- micrograd.cpp
+++ micrograd.cpp
@@ -22,23 +22,48 @@ public:
     this->label = label;
   }
 
+  void make_scalar() { is_scalar = true; }
+
   static std::shared_ptr<Value> create_value(double data, std::string label) {
     return std::make_shared<Value>(data, label);
+  }
+
+  static std::shared_ptr<Value> create_value(
+      double data, std::string label,
+      std::vector<std::shared_ptr<Value>> children,
+      std::function<void(std::vector<std::shared_ptr<Value>>)> backward_fn) {
+    return std::make_shared<Value>(data, label, children, backward_fn);
+  }
+
+  friend std::shared_ptr<Value> operator+(double a,
+                                          const std::shared_ptr<Value> &b) {
+    auto value = create_value(a, std::to_string(a));
+    value->make_scalar();
+    return value + b;
+  }
+
+  friend std::shared_ptr<Value> operator+(const std::shared_ptr<Value> &a,
+                                          double b) {
+    auto value = create_value(b, std::to_string(b));
+    value->make_scalar();
+    return a + value;
   }
 
   friend std::shared_ptr<Value> operator+(const std::shared_ptr<Value> &a,
                                           const std::shared_ptr<Value> &b) {
     auto output_backward = [](std::vector<std::shared_ptr<Value>> children) {
       for (auto &child : children) {
-        child->grad += 1.0;
+        if (!child->is_scalar) {
+          child->grad += 1.0;
+        }
       }
     };
 
     auto output_data = a->data + b->data;
     auto output_label = a->label + "+" + b->label;
     auto output_children = std::vector<std::shared_ptr<Value>>{a, b};
-    return std::make_shared<Value>(output_data, output_label, output_children,
-                                   output_backward);
+    return create_value(output_data, output_label, output_children,
+                        output_backward);
   }
 
   void backward() {
@@ -94,6 +119,7 @@ private:
   double grad = 0.0;
   std::vector<std::shared_ptr<Value>> children;
   std::string label;
+  bool is_scalar = false;
 };
 
 int main() {
@@ -102,7 +128,7 @@ int main() {
   auto b = Value::create_value(2.0, "b");
   auto c = Value::create_value(3.0, "c");
   std::shared_ptr<Value> d = a + b;
-  std::shared_ptr<Value> e = d + c;
+  std::shared_ptr<Value> e = 1 + a + d + c;
   e->backward();
   e->print_network();
   return 0;
